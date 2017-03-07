1610b3cb0589c040ee396acb8d42e5bc1cb0023d
Keishon Smith <keishon104@gmail.com>
2017-03-07 10:52:25 -0500

    testing
    Dev

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 var express = require('express');  // Function requiring the express module
-var bodyParser = require('body-parser') // Requiring body-parser to read input from forms
+var bodyParser = require('body-parser'); // Requiring body-parser to read input from forms
+
+
+
+
 var app = express();  // Setup express app
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+// middleware used to parse post data.
+var urlencodedParser = app.use(bodyParser.urlencoded({ extended: false }));
 
 // Loads the home screen index.html
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/index.html');
-})
+   console.log("Home page requested");
+
+});
 
 // Function handling data sent after submit button is pressed
-app.post('/contact ',  urlencodedParser, function(req, res){
-res.sendFilr(__dirname + '/contact.html');
+app.post('/myaction',  function(req, res) {
+  console.log("The form has been submitted");
+  console.log(req.body);
+  return (true);
+});
 
-})
+
 
 // Server Setup and testing
-app.listen (3000);    // Listens to port 3000
-console.log("You are now listening to port 3000")
+app.listen (8080);    // Listens to port 3000
+console.log("You are now listening to port: 8080");
