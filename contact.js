f79ac1b8e4730621bce0cd694dcae8f5e5b7872a
Keishon Smith <keishon104@gmail.com>
2017-03-22 04:03:28 -0400

    Deleting Files
    Taking out files not essential to the program.

--- contact.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');  // Function requiring the express module
-var bodyParser = require('body-parser') // Requiring body-parser to read input from forms
-var app = express();  // Setup express app
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-console.log("the Process is working!!!");
