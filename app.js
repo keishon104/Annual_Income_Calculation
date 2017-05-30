56eff9d001a56950310ea9103e6cfedb405999f6
Keishon Smith <keishon104@gmail.com>
2017-05-30 01:05:25 -0400

    MongoDB_Development: Pushing month data to backend
    Completed backend pushing data to the MongoDB database.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http'); // requiring http module
 const express = require('express');  // Function requiring the express module
 const bodyParser = require('body-parser'); // Requiring body-parser to read input from forms
-const logic = require('./models/checks'); // imports the check models file
+const Check = require('./models/checks.js'); // imports the check models file
 const app = express();  // Setup express app
 const mongoose = require('mongoose');
 
@@ -13,6 +13,7 @@ mongoose.Promise = global.Promise;
 
 // Allow data to be parsed from input
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Upload index file when home page is requested
 app.get('/', function(req, res) {
@@ -23,13 +24,27 @@ app.get('/', function(req, res) {
 // function called once the submit button is pressed
 app.post('/myaction', function(req, res) {
   console.log(req.body);
-var check = new check ({
-    first: req.body.first,
-    last: req.body.last,
-    month1: req.body.month1
-  }).save(function(err, doc){
-  });
-  res.send("Your data was successfully sent to the database.");
+
+var myData = new Check(req.body);
+
+myData.save()
+    .then(item => {
+      res.send("item saved to database");
+    })
+    .catch(err => {
+      res.status(400).send("Unable to save to database");
+    });
+    console.log("Data successfully saved to database");
+
+// Code below would not work
+  /*
+Check.create(req.body).then(function(Check){
+    res.send(post);
+});
+Check();
+*/
+
+
 });
 
 // Creation of server and port listening
--- models/checks.js
+++ models/checks.js
@@ -1,20 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;  // Stores Schema class
 
+
+module.exports = battery;
+
+var battery = "This is the checks Schema working";
+
 // Create Schema and Model
-const checkSchema = new Schema({
+var CheckSchema = new Schema({
   first: {
     type: String,
     required: [true, 'Name field is required']
   },
-
   last: {
     type: String
   },
   month1: {
     type: Number},
+  month2: Number,
+  month3: Number,
+  month4: Number,
+  month5: Number,
+  month6: Number,
+  month7: Number,
+  month8: Number,
+  month9: Number,
+  month10: Number,
+  month11: Number,
+  month12: Number, 
 });
 
-const check = mongoose.model('check', checkSchema);
+var Check = mongoose.model('Check', CheckSchema);
 
-module.exports = check;
+
+module.exports = Check;
