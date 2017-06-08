6007fbde08307ebb5ae9f90c2546d2cb610be281
Keishon Smith <keishon104@gmail.com>
2017-06-08 11:07:46 -0400

    f

--- app.js
+++ app.js
@@ -9,17 +9,28 @@ const mongoose = require('mongoose');
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/checks');
+
+// ES6 Promise
 mongoose.Promise = global.Promise;
 
-// Allow data to be parsed from input
+// Connection test to database
+mongoose.connection.once('open',function(){
+  console.log("Connection to database has been established.");
+}).on('error', function(error){
+  console.log("connection error", error);
+});
+
+// Data Parserer
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 
 // Upload index file when home page is requested
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
     console.log("The Home Page has been loaded");
 });
+
 
 // function called once the submit button is pressed
 app.post('/myaction', function(req, res) {
@@ -29,23 +40,15 @@ var myData = new Check(req.body);
 
 myData.save()
     .then(item => {
-      res.send("item saved to database");
+      res.sendFile(__dirname + '/final.html');
     })
     .catch(err => {
       res.status(400).send("Unable to save to database");
     });
     console.log("Data successfully saved to database");
 
-// Code below would not work
-  /*
-Check.create(req.body).then(function(Check){
-    res.send(post);
 });
-Check();
-*/
 
-
-});
 
 // Creation of server and port listening
 http.createServer(app).listen(1337, function () {
